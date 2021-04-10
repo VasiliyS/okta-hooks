0a2d3c9900dad72a2b62dff23589403222fde8b8
Vasily Suvorov <vsuvorov@Vasilys-MacBook-Pro.local>
2021-04-10 21:53:16 +0200

    updated POST logic, printing the query and body

--- api/session.ts
+++ api/session.ts
@@ -1,16 +1,25 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req: VercelRequest, res: VercelResponse) {
+  // Okta EventHook verification Logic
   if (req.method === "GET") {
     const returnValue = {
       verification: req.headers["x-okta-verification-challenge"],
     };
-    res.json(returnValue);
+    res.status(200).json(returnValue);
   } else if (req.method === 'POST'){
       console.log('incoming post request')
       console.log(`request parameters ${req.url}`)
-      res.status(200)
+      for(const k in req.query){
+        console.log(`query has key: ${k} = ${req.query[k]}`)
+      }
+      if(req.body != null){ 
+        console.log(" -- body ---")
+        const bodyStr = JSON.stringify(req.body)
+        console.log(bodyStr)
+      }
+      res.status(200).end();
   } else {
-    res.status(404).json({ error: 'This request is not supported' });
+    res.status(404).json({ error: 'This request is not supported' })
   }
 }
