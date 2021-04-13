fd32d7c731e1964cc06e24e3b38510b93c1ed9cf
Vasily Suvorov <vsuvorov@gmail.com>
2021-04-13 22:36:04 +0200

    Sorted out asyncs, improved logging with levels

--- api/session.ts
+++ api/session.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import * as oktaEvent from "@okta/okta-sdk-nodejs/src/types/models/LogEvent"
-import okta = require("@okta/okta-sdk-nodejs");
+import * as oktaEvent from "@okta/okta-sdk-nodejs/src/types/models/LogEvent";
+import okta from "@okta/okta-sdk-nodejs";
 
 // populate Org URL and Token from env variables
 // OKTA_CLIENT_ORGURL=https://dev-1234.oktapreview.com/
@@ -12,7 +12,21 @@ const oktaClient = new okta.Client({
   //cacheMiddleware: null
 });
 
-export default function (req: VercelRequest, res: VercelResponse) {
+//quick and dirty debugging help
+enum LogLevel {
+  Production, // Errors Only
+  Info,
+  Detailed
+}
+const debugLevel = parseInt(process.env.DEBUG_LEVEL || "0", 10);
+const log = (logLevel: LogLevel, ...args) => {
+    if(logLevel >= debugLevel){
+      console.log(...args);
+    }
+};
+
+
+export default async function (req: VercelRequest, res: VercelResponse) {
   // Okta EventHook verification Logic
   if (req.method === "GET") {
     const returnValue = {
@@ -20,24 +34,23 @@ export default function (req: VercelRequest, res: VercelResponse) {
     };
     res.status(200).json(returnValue);
   } else if (req.method === "POST") {
-    console.log("incoming post request");
-    console.log(`request parameters ${req.url}`);
+    log(LogLevel.Detailed, "incoming post request");
+    log(LogLevel.Detailed,`request parameters ${req.url}`);
     // TODO check Authorization - shouldn't process requests not coming from Okta!!
     if (req?.body) {
-      console.log(" -- body ---");
-      const bodyStr = JSON.stringify(req.body, null, 2);
-      console.log(bodyStr);
+      log(LogLevel.Detailed, " --- body ---");
+      log(LogLevel.Detailed, JSON.stringify(req.body, null, 2));
       //look for logout event and suspend user to prevent login
       const events = req.body?.data?.events;
       // Okta can batch events, so if multiple users log out we'll need to 'suspend' them all
-      console.log(`received ${events.length} events`);
-      events?.forEach((event : oktaEvent.LogEvent) => {
-        console.log(
-          `Processing event: ${event.eventType} for user with id: ${event.actor.id}`
+      log(LogLevel.Info,`received ${events.length} events`);
+      events?.forEach(async (event: oktaEvent.LogEvent, num: number) => {
+        log(LogLevel.Info,
+          `Processing event #${num}: ${event.eventType} for user with id: ${event.actor.id}`
         );
         if (event.eventType === "user.session.end") {
-          suspendUser(event.actor.id)
-          .then(res => console.log(`suspendUser returned: ${res}`));
+          let resp = await suspendUser(event.actor.id);
+          log(LogLevel.Info,`suspendUser returned: ${resp}`);
         }
       });
     }
@@ -48,14 +61,15 @@ export default function (req: VercelRequest, res: VercelResponse) {
 }
 
 async function suspendUser(id: string) {
-  console.log(`Trying to suspend a User with id "${id}", OrgUrl is: ${oktaClient.baseUrl}`);
-
+  console.log(LogLevel.Detailed,
+    `Trying to suspend a User with id "${id}", OrgUrl is: ${oktaClient.baseUrl}`
+  );
   try {
     let resp = await oktaClient.suspendUser(id);
-    console.log(`Success suspending User with id "${id}"), response: ${resp} `);
+    log(LogLevel.Detailed,`Success suspending User with id "${id}"), response: ${resp} `);
     return true;
   } catch (err) {
-    console.log(`Error suspending User with id(${id}), received: ${err} `);
+    log(LogLevel.Production,`Error suspending User with id(${id}), received: ${err} `);
     return false;
   }
 }
